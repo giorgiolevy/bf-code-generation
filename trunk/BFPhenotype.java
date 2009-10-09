da0cd9d49f751fcf4c8074a6a12d728f4087133f
gerglion <gerglion@ac699100-af82-11de-ae21-b99107b3ee63>
2009-10-09 22:46:26 +0000

    Added method level comments.  Cleaned up white space problems.

--- trunk/BFPhenotype.java
+++ trunk/BFPhenotype.java
@@ -21,16 +21,38 @@ public class BFPhenotype implements Phenotype {
     }
     // ******************
 
+    /**
+     * ***********************************************
+     * Constructors
+     */
+
     public BFPhenotype(byte[] bytes) {
         bfCode = BFInterpreter.bfToString(bytes);
         output = BFInterpreter.interpret(bfCode);
         fitness = new BFPhenotype.Fitness(output, bfCode.length());
     }
 
+    /**
+     * ***********************************************
+     * Methods
+     */
+
+    /**
+     * Method: getFitness()
+     *
+     *  Accessor method for the object's fitness object.
+     *
+     */
     public Comparable getFitness() {
         return fitness;
     }
 
+    /**
+     * Method: distance()
+     *
+     *  Method to compute difference between output and s1.
+     *
+     */
     public static int distance(String s1) {
         if (s1.length() == 0) {
             return Integer.MAX_VALUE;
@@ -54,14 +76,31 @@ public class BFPhenotype implements Phenotype {
 
     }
 
+    /**
+     * Method: distance()
+     *
+     *  Method to compute difference between output and s1.
+     *
+     */
     public String toString() {
         return fitness + ":" + /*bfCode + ":" + output.length() +*/ ":" + output;
     }
 
+    /**
+     * Private Class: Fitness 
+     *
+     *  Inner class to represent a phenotype's fitness. 
+     *
+     */
     private static class Fitness implements Comparable<Fitness> {
 
         int[] vals;
         int len;
+
+        /**
+         * ***********************************************
+         * Constructors
+         */
 
         public Fitness(String s, int len) {
             while (s.length() < BFInterpreter.desired.length()) {
@@ -69,14 +108,14 @@ public class BFPhenotype implements Phenotype {
             }
 
             vals = new int[s.length()];
-//System.out.println(s);
+            //System.out.println(s);
             for (int i = 0; i < BFInterpreter.desired.length(); i++) {
                 vals[i] = (BFInterpreter.desired.charAt(i) - s.charAt(i));
-if (vals[i]<256 && vals[i]>-256)
-                vals[i] = vals[i] * vals[i];
-else
-vals[i]=Integer.MAX_VALUE;
-if (vals[i]<0)System.out.println("problem"+(int)BFInterpreter.desired.charAt(i)+":"+(int)s.charAt(i));
+                if (vals[i]<256 && vals[i]>-256)
+                  vals[i] = vals[i] * vals[i];
+                else
+                  vals[i]=Integer.MAX_VALUE;
+                if (vals[i]<0)System.out.println("problem"+(int)BFInterpreter.desired.charAt(i)+":"+(int)s.charAt(i));
             }
             for (int i = BFInterpreter.desired.length(); i < s.length(); i++) {
                 vals[i] = Integer.MAX_VALUE;
@@ -85,37 +124,54 @@ if (vals[i]<0)System.out.println("problem"+(int)BFInterpreter.desired.charAt(i)+
             this.len = len;
         }
 
-        public int compareTo(Fitness o) {
-if (len>2000)
-return -1;
-            int[] otherVal = o.vals;
-            for (int i = 0; i < otherVal.length && i < vals.length; i++) {
-		//System.out.println("abc");
-                if (vals[i] > otherVal[i]) {
-                    return -1;
-                } else if (vals[i] < otherVal[i]) {
-                    return 1;
-                }
-            }
+        /**
+         * ***********************************************
+         * Methods
+         */
 
-            if (vals.length > BFInterpreter.desired.length()) {
-		//System.out.println("here");
+        /**
+         * Method: compareTo()
+         *
+         *  Overriden method to make fitness objects comparable. 
+         *
+         */
+        public int compareTo(Fitness o) {
+          if (len>2000)
+            return -1;
+          int[] otherVal = o.vals;
+          for (int i = 0; i < otherVal.length && i < vals.length; i++) {
+            //System.out.println("abc");
+            if (vals[i] > otherVal[i]) {
                 return -1;
-            } else if (vals.length < BFInterpreter.desired.length()) {
-		//System.out.println("there");
+            } else if (vals[i] < otherVal[i]) {
                 return 1;
             }
+          }
 
-       //     if (Math.random() < .8) {
-                return (o.len - len);
-         //   } else {
-           //     return (len - o.len);
-            //}
+          if (vals.length > BFInterpreter.desired.length()) {
+            //System.out.println("here");
+            return -1;
+          } else if (vals.length < BFInterpreter.desired.length()) {
+            //System.out.println("there");
+            return 1;
+          }
+
+          //if (Math.random() < .8) {
+            return (o.len - len);
+          //} else {
+            //return (len - o.len);
+          //}
 
         }
 
+        /**
+         * Method: toString()
+         *
+         *  Overriden toString() method. 
+         *
+         */
         public String toString() {
-            return "(" + Arrays.toString(vals) + ":" + len + ")";
+          return "(" + Arrays.toString(vals) + ":" + len + ")";
         }
     }
 }
